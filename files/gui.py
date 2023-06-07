50c6d42d6c50aa3e4f774c7145d4304acf97dafd
Disha <disha01423@gmail.com>
2023-06-07 23:48:03 +0530

    Change theme,add clock,handle edit and complete error

--- files/gui.py
+++ files/gui.py
@@ -1,6 +1,10 @@
 import python12
 import PySimpleGUI as sg
+import time
 
+sg.theme("Black")
+
+clock = sg.Text('', key = "clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = "Enter todo", key = "todo")
 add_button = sg.Button("Add")
@@ -19,16 +23,16 @@ exit_button = sg.Button("Exit")
 # layout = [[label] , [input_box, add_button],[list_box,edit_button]]
 
 window = sg.Window("My To-Do App",
-                   layout = [[label] ,
+                   layout = [[clock],
+                             [label] ,
                              [input_box, add_button],
                              [list_box, edit_button, complete_button],
                              [exit_button]],
                    font = ('Helvetica',20))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    event, values = window.read(timeout = 200)
+    window["clock"].update(value = time.strftime("%b %d,%Y %H:%M:%S"))
+
     match event:
         case 'Add':
             todos = python12.get_todos()
@@ -38,21 +42,27 @@ while True:
             window['todos'].update(values=todos)
 
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
-            todos = python12.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            python12.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = python12.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                python12.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font =("Helvetica", 20) )
 
         case 'Complete':
-            todo_to_complete = values['todos'][0]
-            todos = python12.get_todos()
-            todos.remove(todo_to_complete)
-            python12.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = python12.get_todos()
+                todos.remove(todo_to_complete)
+                python12.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font =("Helvetica", 20) )
 
         case 'Exit':
             break
@@ -64,12 +74,4 @@ while True:
             break
 print("hello")
 window.close()
-# label1 = sg.Text("Enter feel:")
-# input1 = sg.Input()
-# label2 = sg.Text("Enter inches:")
-# input2 = sg.Input()
-# convert_button = sg.FilesBrowse("Convert")
-#
-# windows = sg.Window("Convertor", layout = [[label1, input1],[label2, input2],[convert_button]])
-# windows.read()
-# windows.close()
+
